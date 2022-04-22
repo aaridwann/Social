324b5223c6140858551b2d701e3bcfe8ce9abe66
aaridwann <aaridwann@gmail.com>
2022-04-22 19:01:57 +0700

    test

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,5 @@
 import React, { Suspense, useContext, useState } from "react";
-// import HeroSect from "./Components/HeroSection/HeroSect";
 import Navbar from "./Components/Navbar/Navbar";
-// import Products from "./Components/Products/Products";
-// import Profile from "./Components/Profile/Profile";
 import { AuthContext } from "./Context/AuthContext";
 import Login from "./Login";
 const HeroSect = React.lazy(() => import('./Components/HeroSection/HeroSect'))
@@ -12,20 +9,20 @@ const Profile = React.lazy(() => import('./Components/Profile/Profile'))
 const loading = <h1 className="text-center text-4xl font-bold text-matcha-dark">Loading...</h1>
 
 export default function Home() {
-    const {Auth, setAuth} = useContext(AuthContext)
-    const [modalLogin, setModalLogin] = useState(false)
-    const change = () => setModalLogin(!modalLogin)
-    return (
+  const { Auth, setAuth } = useContext(AuthContext)
+  const [modalLogin, setModalLogin] = useState(false)
+  const change = () => setModalLogin(!modalLogin)
+  return (
     <div>
-    <title>Home</title>
-      <Navbar className='fixed'  />
+      <title>Home</title>
+      <Navbar className='fixed' />
       <Suspense fallback={loading}>
-      <section>
-      <HeroSect change={change} />
-      {!Auth && modalLogin && <Login change={change} />}
-      <Profile />
-      <Products />
-      </section>
+        <section>
+          <HeroSect change={change} />
+          {!Auth && modalLogin && <Login change={change} />}
+          <Profile />
+          <Products />
+        </section>
       </Suspense>
     </div>
   );
